6b1e10a9e99f499144faeabad74e02398dfd27b8
xzx <549366192@qq.com>
2021-07-16 14:58:22 +0800

    '1'

---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="PostList">
+    <!--在数据未返回时，显示正在加载的图片 -->
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/loading.gif" >
+    </div>
+    <!--代表帖子列表-->
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  data(){
+    return {
+      isLoading:false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$http=Axios;
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
